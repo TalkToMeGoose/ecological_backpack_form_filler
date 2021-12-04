54949c4ea75ee8ec44a03e47c4f369fcc3f93ed6
Adam Hardaker <michael.a.hardaker@gmail.com>
2021-12-04 15:01:47 +0100

    initializing thesis python project

---
+++ read_input_scrape.py
@@ -0,0 +1,19 @@
+'''
+
+The purpose of this is to:
+1) take values out of a csv file,
+2) plug them into and online survey
+link: https://www.ressourcen-rechner.de/calculator.php?lang=en
+3) scrape the result
+4) insert back into python
+
+'''
+
+'''
+things I might need
+import openpyxl
+import beautifulsoup
+import selenium
+import pandas
+'''
+if 
